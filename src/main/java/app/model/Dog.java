5d3dd88cf4f93a529b5b4a88eaa67496fdb43928
Vlad <124003398+goodbye-322@users.noreply.github.com>
2023-07-15 02:10:08 +0300

    2.1.3

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat extends Animal {
+@Qualifier("dog")
+public class Dog extends Animal {
     @Override
     public String toString() {
         return "Im a Cat";
